1f079257e78833d850d6e199b4bb20311236a2fd
rusGoldberg <92647335+rusGoldberg@users.noreply.github.com>
2025-04-16 20:58:20 +0300

    Update to_ru.cob

--- to_ru.cob
+++ to_ru.cob
@@ -12,3 +12,121 @@ WORKING-STORAGE SECTION.
 01 WS-INDEX PIC 9(3) VALUE 1.
 01 WS-LENGTH PIC 9(3).
 
+01 MORSE-TABLE.
+   05 FILLER PIC X(2) VALUE "А ".
+   05 FILLER PIC X(5) VALUE ".-    ".
+   05 FILLER PIC X(2) VALUE "Б ".
+   05 FILLER PIC X(5) VALUE "-...  ".
+   05 FILLER PIC X(2) VALUE "В ".
+   05 FILLER PIC X(5) VALUE ".--   ".
+   05 FILLER PIC X(2) VALUE "Г ".
+   05 FILLER PIC X(5) VALUE "--.   ".
+   05 FILLER PIC X(2) VALUE "Д ".
+   05 FILLER PIC X(5) VALUE "-..   ".
+   05 FILLER PIC X(2) VALUE "Е ".
+   05 FILLER PIC X(5) VALUE ".     ".
+   05 FILLER PIC X(2) VALUE "Ё ".
+   05 FILLER PIC X(5) VALUE ".     ".
+   05 FILLER PIC X(2) VALUE "Ж ".
+   05 FILLER PIC X(5) VALUE "...-- ".
+   05 FILLER PIC X(2) VALUE "З ".
+   05 FILLER PIC X(5) VALUE "--..  ".
+   05 FILLER PIC X(2) VALUE "И ".
+   05 FILLER PIC X(5) VALUE "..    ".
+   05 FILLER PIC X(2) VALUE "Й ".
+   05 FILLER PIC X(5) VALUE ".---  ".
+   05 FILLER PIC X(2) VALUE "К ".
+   05 FILLER PIC X(5) VALUE "-.-   ".
+   05 FILLER PIC X(2) VALUE "Л ".
+   05 FILLER PIC X(5) VALUE ".-..  ".
+   05 FILLER PIC X(2) VALUE "М ".
+   05 FILLER PIC X(5) VALUE "--    ".
+   05 FILLER PIC X(2) VALUE "Н ".
+   05 FILLER PIC X(5) VALUE "-.    ".
+   05 FILLER PIC X(2) VALUE "О ".
+   05 FILLER PIC X(5) VALUE "---   ".
+   05 FILLER PIC X(2) VALUE "П ".
+   05 FILLER PIC X(5) VALUE ".--.  ".
+   05 FILLER PIC X(2) VALUE "Р ".
+   05 FILLER PIC X(5) VALUE ".-.   ".
+   05 FILLER PIC X(2) VALUE "С ".
+   05 FILLER PIC X(5) VALUE "...   ".
+   05 FILLER PIC X(2) VALUE "Т ".
+   05 FILLER PIC X(5) VALUE "-     ".
+   05 FILLER PIC X(2) VALUE "У ".
+   05 FILLER PIC X(5) VALUE "..-   ".
+   05 FILLER PIC X(2) VALUE "Ф ".
+   05 FILLER PIC X(5) VALUE "..-.  ".
+   05 FILLER PIC X(2) VALUE "Х ".
+   05 FILLER PIC X(5) VALUE "....  ".
+   05 FILLER PIC X(2) VALUE "Ц ".
+   05 FILLER PIC X(5) VALUE "-.-.  ".
+   05 FILLER PIC X(2) VALUE "Ч ".
+   05 FILLER PIC X(5) VALUE "---.  ".
+   05 FILLER PIC X(2) VALUE "Ш ".
+   05 FILLER PIC X(5) VALUE "----  ".
+   05 FILLER PIC X(2) VALUE "Щ ".
+   05 FILLER PIC X(5) VALUE "--.-  ".
+   05 FILLER PIC X(2) VALUE "Ъ ".
+   05 FILLER PIC X(5) VALUE "--.-- ".
+   05 FILLER PIC X(2) VALUE "Ы ".
+   05 FILLER PIC X(5) VALUE "-.--. ".
+   05 FILLER PIC X(2) VALUE "Ь ".
+   05 FILLER PIC X(5) VALUE "-..-  ".
+   05 FILLER PIC X(2) VALUE "Э ".
+   05 FILLER PIC X(5) VALUE "..-.. ".
+   05 FILLER PIC X(2) VALUE "Ю ".
+   05 FILLER PIC X(5) VALUE "..--  ".
+   05 FILLER PIC X(2) VALUE "Я ".
+   05 FILLER PIC X(5) VALUE ".-.-  ".
+   05 FILLER PIC X(2) VALUE "1 ".
+   05 FILLER PIC X(5) VALUE ".---- ".
+   05 FILLER PIC X(2) VALUE "2 ".
+   05 FILLER PIC X(5) VALUE "..--- ".
+   05 FILLER PIC X(2) VALUE "3 ".
+   05 FILLER PIC X(5) VALUE "...-- ".
+   05 FILLER PIC X(2) VALUE "4 ".
+   05 FILLER PIC X(5) VALUE "....- ".
+   05 FILLER PIC X(2) VALUE "5 ".
+   05 FILLER PIC X(5) VALUE "..... ".
+   05 FILLER PIC X(2) VALUE "6 ".
+   05 FILLER PIC X(5) VALUE "-.... ".
+   05 FILLER PIC X(2) VALUE "7 ".
+   05 FILLER PIC X(5) VALUE "--... ".
+   05 FILLER PIC X(2) VALUE "8 ".
+   05 FILLER PIC X(5) VALUE "---.. ".
+   05 FILLER PIC X(2) VALUE "9 ".
+   05 FILLER PIC X(5) VALUE "----. ".
+   05 FILLER PIC X(2) VALUE "0 ".
+   05 FILLER PIC X(5) VALUE "----- ".
+   05 FILLER PIC X(2) VALUE " ".
+   05 FILLER PIC X(5) VALUE "| ".
+   05 FILLER PIC X(2) VALUE ".".
+   05 FILLER PIC X(5) VALUE ".-.-.- ".
+   05 FILLER PIC X(2) VALUE ",".
+   05 FILLER PIC X(5) VALUE "--..-- ".
+   05 FILLER PIC X(2) VALUE "?".
+   05 FILLER PIC X(5) VALUE "..--.. ".
+   05 FILLER PIC X(2) VALUE "'".
+   05 FILLER PIC X(5) VALUE ".----. ".
+   05 FILLER PIC X(2) VALUE "!".
+   05 FILLER PIC X(5) VALUE "-.-.-- ".
+   05 FILLER PIC X(2) VALUE "/".
+   05 FILLER PIC X(5) VALUE "-..-.".
+   05 FILLER PIC X(2) VALUE "(".
+   05 FILLER PIC X(5) VALUE "-.--.".
+   05 FILLER PIC X(2) VALUE ")".
+   05 FILLER PIC X(5) VALUE "-.--.-".
+   05 FILLER PIC X(2) VALUE "&".
+   05 FILLER PIC X(5) VALUE ".-...".
+   05 FILLER PIC X(2) VALUE ":".
+   05 FILLER PIC X(5) VALUE "---...".
+   05 FILLER PIC X(2) VALUE ";".
+   05 FILLER PIC X(5) VALUE "-.-.-.".
+   05 FILLER PIC X(2) VALUE "=".
+   05 FILLER PIC X(5) VALUE "-...-".
+
+PROCEDURE DIVISION.
+   DISPLAY "Enter text: " WITH NO ADVANCING.
+   ACCEPT WS-TEXT.
+   MOVE FUNCTION LENGTH(WS-TEXT) TO WS-LENGTH.
